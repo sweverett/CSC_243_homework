9e284f33d6afd664e8c67fea38c591b8aeb0a7da
Spencer Everett <spencerweverett@gmail.com>
2015-01-14 12:38:27 -0600

    hw2 v0

---
+++ spencer_everett_csc243hw2.py
@@ -0,0 +1,30 @@
+# CSC 243-501
+# Assignment 2 template
+# Put your name here
+
+# List your collaborator(s) here (no more than two other people)
+# Include a comment that provides significant details about each
+# person's contribution to the assignment
+
+# If you did not collaborate with anyone, place a comment in the
+# file indicating that
+
+# Files without this information will earn a 0
+
+# Include doc strings for full credit
+
+# Question 1
+def maxlen(s):
+    pass
+    
+# Question 2
+def customSpam(name, amount, address):
+    pass
+
+# Question 3
+def countWords(file, num):
+    pass
+
+# Question 4
+def distribution(fname):
+    pass
